cd7cf413b5a3416ed3cb7e918dbd05bdc7f86e03
Sean Nealon <signalflowsean@gmail.com>
2018-10-29 15:47:12 -0400

    added bcrypjs

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const mongoose = require('mongoose'); 
+const bcrypt = require('bcryptjs'); 
 
 const userSchema = mongoose.Schema({ 
   fullname : { type : String}, 
@@ -18,8 +19,12 @@ userSchema.set('toJSON', {
 });
 
 userSchema.methods.validatePassword = function (incomingPassword) {
-  const user = this; // for clarity
-  return incomingPassword === user.password;
+  return bcrypt.compare(incomingPassword, this.password);
+};
+
+userSchema.statics.hashPassword = function (incomingPassword) {
+  const digest = bcrypt.hash(incomingPassword, 10);
+  return digest;
 };
 
 module.exports = mongoose.model('User', userSchema); 
--- routes/users.js
+++ routes/users.js
@@ -8,17 +8,27 @@ const router = express.Router();
 router.post('/', (req, res, next) => { 
   const {fullname, username, password} = req.body; 
   const newUser = {fullname, username, password}; 
-  console.log('username:', username);
   
-  User
-    .create(newUser)
-    .then(result => { 
-      res
+  return User.hashPassword(password)
+    .then(digest => { 
+      const newUser = { 
+        username, 
+        password: digest, 
+        fullname
+      }; 
+      return User.create(newUser); 
+    })
+    .then (result => { 
+      return res
         .status(201)
         .location(`/api/users/${result.id}`)
         .json(result); 
     })
     .catch(err => { 
+      if(err.code === 11000) { 
+        err = new Error('The username already exists'); 
+        err.status = 400; 
+      }
       return next(err); 
     }); 
 }); 
