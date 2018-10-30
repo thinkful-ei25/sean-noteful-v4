715ddfe541ca1e25065ed5b1e8d3789019b3a2ca
Sean Nealon <signalflowsean@gmail.com>
2018-10-30 14:46:18 -0400

    added JWT to /routes/auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 'use strict';
-
+const {JWT_SECRET, JWT_EXPIRY} = require('../config'); 
+const jwt = require('jsonwebtoken'); 
 const passport = require('passport'); 
 const express = require('express'); 
 
@@ -8,8 +9,17 @@ const router = express.Router();
 const options = {session: false, failWithError: true};
 const localAuth = passport.authenticate('local', options);
 
+function createAuthToken (user) { 
+  return jwt.sign({ user }, JWT_SECRET, {
+    subject: user.username,
+    expiresIn: JWT_EXPIRY
+  });
+}
+
 router.post('/', localAuth, function (req, res) {
-  return res.json(req.user);
+  const authToken = createAuthToken(req.user.toJSON()); 
+  return res.json({ authToken });
 });
+
 
 module.exports = router; 
